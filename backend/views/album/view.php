f258835f44a81a9ec4e26ff309a6925480125d20
DiogoC98 <diogocruz12345@hotmail.com>
2018-10-24 16:12:33 +0100

    ertyui

--- backend/views/album/view.php
+++ backend/views/album/view.php
@@ -23,6 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'method' => 'post',
             ],
         ]) ?>
+        <?= Html::a('Ver Comentários', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
     </p>
 
     <?= DetailView::widget([
