8e2087c1cf14ebdae4b5991a53d36dd6ae837516
BrunoCRocha <2170323@my.ipleiria.pt>
2018-12-04 15:09:26 +0000

    home

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,5 +3,5 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-Yii::setAlias('@capas', dirname(dirname(__DIR__)).'\frontend\web\img\capas');
+Yii::setAlias('@capas', dirname(dirname(__DIR__)).'\common\img\capas');
 
