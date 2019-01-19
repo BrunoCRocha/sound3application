4fef6839afe0c72e0de6fd13753f43b3e7c658e7
DiogoC98 <diogocruz12345@hotmail.com>
2019-01-19 20:32:05 +0000

    dados

--- frontend/modules/api/controllers/FavalbumController.php
+++ frontend/modules/api/controllers/FavalbumController.php
@@ -4,10 +4,88 @@ namespace frontend\modules\api\controllers;
 
 use common\models\Album;
 use common\models\Artista;
+use common\models\Fav_Album;
 use common\models\Fav_Artista;
+use common\models\Fav_Musica;
 use yii\filters\auth\HttpBasicAuth;
 
 class FavalbumController extends \yii\rest\ActiveController
 {
     public $modelClass = 'common\models\Fav_Album';
+
+
+    //Verifica se o Album está nos Favoritos
+    public function actionFindfavalbum($userId, $albumId){
+        $fav = Fav_Album::find()
+            ->where(['and',['id_utilizador' => $userId, 'id_album' => $albumId]])
+            ->one();
+
+        if($fav != null){
+            return true;
+        }
+        return false;
+    }
+
+    //Verifica as músicas Do Album se estão nos Favoritos
+    public function actionFindfavmusica($userId, $musicaId){
+        $fav = Fav_Musica::find()
+            ->where(['and',['id_utilizador' => $userId, 'id_musica' => $musicaId]])
+            ->one();
+
+        if($fav != null){
+            return true;
+        }
+        return false;
+    }
+
+    //Criar Favorito Album
+    public function actionCriarfavoritoalbum(){
+        $idUser = \Yii::$app->request->post('id_utilizador');
+        $idAlbum = \Yii::$app->request->post('id_album');
+
+        $climodel = new Fav_Album();
+        $climodel->id_utilizador = $idUser;
+        $climodel->id_album = $idAlbum;
+
+        $ret = $climodel->save();
+
+        return $ret;
+    }
+
+
+    //Vai Buscar todos os Favoritos do User
+    public function actionGetallalbunsfavoritos($userId){
+         $favAlbum = Fav_Album::find()
+            ->where(['id_utilizador' => $userId])
+            ->all();
+
+        $album =array();
+        foreach ($favAlbum as $favorito){
+            array_push($album, Album::find()
+                ->where(['id' => $favorito->id_album])
+                ->one());
+        }
+
+        return $album;
+    }
+
+    //Vais Buscar 5 Albuns aos Favoritos para Mostrar na Atividade dos Favoritos
+    public function actionGetalbunsfavoritos($userId){
+        $favAlbum = Fav_Album::find()
+            ->where(['id_utilizador' => $userId])
+            ->all();
+
+        $favAlbum = array_slice($favAlbum,0, 5, true);
+
+        $albuns =array();
+        foreach ($favAlbum as $favorito){
+            array_push($albuns, Album::find()
+                ->where(['id' => $favorito->id_album])
+                ->one());
+        }
+
+        return $albuns;
+    }
+
+
 }
--- frontend/modules/api/controllers/FavartistaController.php
+++ frontend/modules/api/controllers/FavartistaController.php
@@ -10,4 +10,40 @@ use yii\filters\auth\HttpBasicAuth;
 class FavartistaController extends \yii\rest\ActiveController
 {
     public $modelClass = 'common\models\Fav_Artista';
+
+    //Vai Buscar todos os artistas nos Favoritos do User
+    public function actionGetallartistasfavoritos($userId){
+        $favArtista = Fav_Artista::find()
+            ->where(['id_utilizador' => $userId])
+            ->all();
+
+        $artista =array();
+        foreach ($favArtista as $favorito){
+            array_push($artista, Artista::find()
+                ->where(['id' => $favorito->id_artista])
+                ->one());
+        }
+
+        return $artista;
+    }
+
+    //SO 5 ALBUNS PARA MOSTRAR NA VIEW FAVORITOS
+    public function actionGetartistasfavoritos($userId){
+        $favArtista = Fav_Artista::find()
+            ->where(['id_utilizador' => $userId])
+            ->all();
+
+        $favArtista = array_slice($favArtista,0, 5, true);
+
+        $artistas = array();
+        foreach ($favArtista as $favorito){
+            array_push($artistas, Artista::find()
+            ->where(['id' => $favorito->id_artista])
+                ->one());
+        }
+
+        return $artistas;
+    }
+
+
 }
